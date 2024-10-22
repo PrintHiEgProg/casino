7a7e2266d737f40ae7103f59445f650fd779a2f3
PrintHiEgProg <egorkobzev23gmail.com>
2024-10-22 08:18:15 +0300

    fix frameborder

--- src/SlotStarWars.js
+++ src/SlotStarWars.js
@@ -6,6 +6,7 @@ function SlotStarWars() {
       <iframe
         className="iframe-page"
         src="https://casino-slot-star-wars-game.vercel.app/"
+        frameborder="0"
       ></iframe>
     </div>
   );
